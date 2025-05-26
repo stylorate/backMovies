bec83700707d12eeba626ec75a4817eb0e233578
Javier lozano <javieresticson.lozano@externos.tinsa.com>
2025-05-27 00:27:27 +0200

    Entrega actividad de transferencia

--- src/main/java/com/example/demo/api/MovieController.java
+++ src/main/java/com/example/demo/api/MovieController.java
@@ -6,6 +6,7 @@ import com.example.demo.api.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/movies")
 public class MovieController {
--- src/main/java/com/example/demo/api/persistence/repository/MovieRepository.java
+++ src/main/java/com/example/demo/api/persistence/repository/MovieRepository.java
@@ -1,7 +1,6 @@
 package com.example.demo.api.persistence.repository;
 
 import com.example.demo.api.persistence.entity.Movie;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +9,5 @@ import java.util.Optional;
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
 
-    @EntityGraph(attributePaths = {"actores", "generos"})
     Optional<Movie> findById(Integer id);
 }
